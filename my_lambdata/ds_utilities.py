46b2c4aabc2a4968162751e7ee36720da76e6c17
andronikmk <andronikmk@gmail.com>
2020-07-07 23:33:17 -0400

    Completed initial packaging in class.

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 def enlarge(n):
     ''' This function multiples the input by 100. '''
     return n*100
 
-y = int(input("Enter a number: "))
-print(y, enlarge(y))
+if __name__=='__main__':
+    y = int(input("Enter a number: "))
+    print(y, enlarge(y))
---
+++ my_lambdata/test_function.py
@@ -0,0 +1,4 @@
+from my_lambdata.ds_utilities import enlarge
+
+y = 10
+print(y, enlarge(y))
